52f3c022a6d650a361e6200af832582dfb53e4fb
Quent <qsarrazin35@gmail.com>
2020-09-30 11:01:07 +0200

    Methode invocation

---
+++ Modele/src/ActiveObject/GetValue.java
@@ -0,0 +1,15 @@
+package ActiveObject;
+
+import java.util.concurrent.Callable;
+
+public class GetValue implements Callable<Integer> {
+
+    public GetValue(){
+
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return null;
+    }
+}
--- Modele/src/ActiveObject/MethodInvocation.java
+++
@@ -1,8 +0,0 @@
-package ActiveObject;
-
-import java.util.concurrent.Callable;
-
-public interface MethodInvocation extends Callable<Void> {
-
-
-}
